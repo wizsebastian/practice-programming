41e3938767549ac723a9485d7afa0f4cdb748ac7
Luis Vasquez <wizsebastian@gmail.com>
2024-07-27 17:16:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 22;
+
+function calculo(texto, result) {
+    console.log(texto, result)
+}
+
+calculo("Suma: ", a + b)
+calculo("Resta: ", b - a)
+calculo("Multiplicacion: ", a * b)
+calculo("Division: ", b / a)
